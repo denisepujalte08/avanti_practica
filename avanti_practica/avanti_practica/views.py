eb80d71f09a585079587165e589754c1fb5e1032
Denise <pujalte64@gmail.com>
2025-11-02 17:04:00 -0300

    agregue el context en a ultima linea

--- avanti_practica/avanti_practica/views.py
+++ avanti_practica/avanti_practica/views.py
@@ -7,4 +7,14 @@ def acerca(request):
     return render(request, "acerca.html")
 
 def login(request):
-    return render(request, "login.html")
+    context = {}
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        if email and password:
+            context["mensaje"] = f"Bienvenido, {email}!"
+        else: 
+            context["mensaje"] = f"Debe completar ambos campos!"
+    # ESTO HABIA CAMBIADO
+    return render(request, "login.html", context)
